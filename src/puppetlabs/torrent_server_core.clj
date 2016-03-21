fb091bea860ad69078c464def704f5aabd738043
Michael Smith <michael.smith@puppetlabs.com>
2016-03-21 16:44:20 -0700

    Add client to seed file

--- src/puppetlabs/torrent_server_core.clj
+++ src/puppetlabs/torrent_server_core.clj
@@ -2,18 +2,30 @@
   (:require [clojure.tools.logging :as log]
             [clojure.java.io :as io])
   (:import
-    (java.net InetSocketAddress URI)
+    (java.net InetSocketAddress InetAddress URI)
+    (com.turn.ttorrent.client Client SharedTorrent)
     (com.turn.ttorrent.tracker Tracker TrackedTorrent)
     (com.turn.ttorrent.common Torrent)
     ))
 
 (def file-location "tmp")
-(def localhost (URI. "localhost:6969"))
+(def localhost (InetAddress/getLocalHost))
+(def announce-uri (URI. (str "http://" (.getHostName localhost) ":6969/announce")))
+
+(defn seed-torrent
+  "Start sharing the specified torrent"
+  [torrent]
+  (let [shared-torrent (SharedTorrent. torrent (io/file file-location) true)
+        client (Client. localhost shared-torrent)]
+    (.share client)))
 
 (defn create-torrent
-  "Generate a torrent object for specified file"
-  [file]
-  (Torrent/create (io/file file-location file) localhost "torrent-server"))
+  "Generate a torrent object for specified file, announce to tracker and start seeding"
+  [file tracker]
+  (let [torrent (Torrent/create (io/file file-location file) announce-uri "torrent-server")]
+    (.announce tracker (TrackedTorrent. torrent))
+    (seed-torrent torrent)
+    torrent))
 
 (defn get-torrent
   "Return torrent for file"
@@ -25,7 +37,6 @@
         torrentByName (first (filter #(= file (.getName %)) trackedTorrents))
         torrent (if (nil? torrentByName)
                   (do (log/info "Adding torrent for" file)
-                      (create-torrent file))
+                      (create-torrent file tracker))
                   torrentByName)]
-    (.announce tracker (TrackedTorrent. torrent))
     (io/input-stream (.getEncoded torrent))))
